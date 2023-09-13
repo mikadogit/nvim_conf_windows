cc8211183599266d93c7de575972d3f03423ea31
Mikael <mikael.jouans@thalesaleniaspace.com>
2023-09-13 11:12:46 +0200

    mapping updated

--- Users_AppData_Local_nvim/lua/core/mappings.lua
+++ Users_AppData_Local_nvim/lua/core/mappings.lua
@@ -262,6 +262,7 @@ M.telescope = {
     ["<leader>ff"] = { "<cmd> Telescope find_files <CR>", "Find files" },
     ["<leader>fa"] = { "<cmd> Telescope find_files follow=true no_ignore=true hidden=true <CR>", "Find all" },
     ["<leader>fw"] = { "<cmd> Telescope live_grep <CR>", "Live grep" },
+    ["<leader>fc"] = { "<cmd> Telescope grep_string <CR>", "Grep word under cursor"},
     ["<leader>fb"] = { "<cmd> Telescope buffers <CR>", "Find buffers" },
     ["<leader>fh"] = { "<cmd> Telescope help_tags <CR>", "Help page" },
     ["<leader>fo"] = { "<cmd> Telescope oldfiles <CR>", "Find oldfiles" },
